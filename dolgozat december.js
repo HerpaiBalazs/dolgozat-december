347688b8c0e626eda871be8d7ef200ef3d52374c
Herpai Balazs <116707173+HerpaiBalazs@users.noreply.github.com>
2024-12-03 13:28:03 +0100

    Update dolgozat december.js

--- dolgozat december.js
+++ dolgozat december.js
@@ -1,12 +1,11 @@
 function isTriangle(a,b,c)
 {
-    if(a>0 && b>0 && c>0 && a+b>c && a+c>b && b && b+c>a){
+    if(a>0 && b>0 && c>0 && a+b>c && a+c>b && b+c>a){
         return true
     }
     else{
         return false;
-    }
-   
+    }   
 }
 
 function createArrayOfTiers(num)
